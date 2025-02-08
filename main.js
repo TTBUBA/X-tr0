9bc778e1b7a42e3d47931c5018b9e4fc3a0f97d0
BUBA <122395887+TTBUBA@users.noreply.github.com>
2025-02-08 12:27:59 +0100

    X@tr0
    start

---
+++ main.js
@@ -0,0 +1,31 @@
+var NameUser = "BUBA";
+var Password = "ciao";
+const gallery = document.getElementById('imageGallery');
+
+function Login()
+{
+    const Name = document.getElementById("Username").value;
+    const pass =  document.getElementById("Pass").value;
+
+    if(Name == NameUser && pass == Password)
+    {
+        window.location.href = 'Main.html';
+    }
+}
+
+function loadImage()
+{
+    const imagePath = Array.from({length: 195}, (_, i) => 
+        `Photo/${i + 1}.jpg`);
+
+    imagePath.forEach(path => {
+        const item = document.createElement('div');
+        item.className = 'gallery-item';
+        const img = document.createElement('img');
+        img.src = path;
+        gallery.appendChild(item);
+        item.appendChild(img);
+    });
+}
+
+document.addEventListener('DOMContentLoaded',loadImage)
