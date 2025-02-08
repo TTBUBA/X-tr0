b9ee1a6fcaea73e66a94e6b900722d04c8b8df75
BUBA <122395887+TTBUBA@users.noreply.github.com>
2025-02-08 12:32:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function Login()
 
     if(Name == NameUser && pass == Password)
     {
-        window.location.href = 'Main.html';
+        window.location.href = 'https://ttbuba.github.io/X-tr0/Main.html';
     }
 }
 
